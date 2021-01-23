71940bac03059f6c8e90011c47c93f78d50050c0
Carlicia Smith <smith.carlicia@gmail.com>
2021-01-23 17:39:43 -0500

    add utils folder

---
+++ src/utils/API.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  getRandomEmployee: function () {
+    return axios.get("https://randomuser.me/api/?results=50");
+  },
+};
+
+// export default {
+//     axios("https://randomuser.me/api/")
+//  ((response) => {
+//     console.log(response);
+//   }, (error) => {
+//     console.log(error);
+//   })
+// };
+
+// getUsers: function() {
+//     return axios.get("https://randomuser.me/api/?results=200&nat=us")
+// }
