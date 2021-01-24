ea35aa7ef1052ee217ae08e2c6d47c19213f736c
Carlicia Smith <smith.carlicia@gmail.com>
2021-01-24 10:04:19 -0500

    added DataArea to code

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import DataTable from './Table/DataTable.jsx';
+import DataArea from './Table/DataArea.jsx';
 
 function Search() {
-    // handleSort => {
-    //     if (this.state.order === "descend") {
-    //       this.setState({
-    //         order: "ascend"
-    //       })
-    //     } else {
-    //       this.setState({
-    //         order: "descend"
-    //       })
-    //     }
+
     return (
         <div className="Search-container">
             <input className="search-input">
             </input>
-        </div>
+            <button className="searchButton btn-primary mb-2" type="submit">Search</button> */
+             <DataTable>
+                 <DataArea />
+             </DataTable>
+         </div>
+        
     )
 }
 
 export default Search;
+
+// / class Button extends React.Component{
+    //     return(
+    //         <div className="button">
+    //             <button className="searchButton btn-primary mb-2" type="submit">Search</button>
+    //         </div>
+    //     )
+    // }{props.dataNameInProps}
