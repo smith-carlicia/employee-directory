a147d11fe1c0e314fafb0bb151d0ce2514821da7
Carlicia Smith <smith.carlicia@gmail.com>
2021-01-23 19:40:05 -0500

    resolving user and value undefinded error

--- src/components/Table/DataBody.jsx
+++ src/components/Table/DataBody.jsx
@@ -1,17 +1,17 @@
 import React, { useReducer } from "react";
-import API from './utils/API.jsx';
+import API from '../../utils/API.jsx';
 
 function DataBody() {
     console.log(API);
-    return(
-        <tbody>
+    return(value.map(user=>(
+        <tbody key={user}>
             <td>{user.picture}</td>
             <td>{user.name}</td>
             <td>{user.cell}</td>
             <td>{user.email}</td>
             <td>{user.dob}</td>
         </tbody>
-    )
+    )))
 }
 
 export default DataBody;
